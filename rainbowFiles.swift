b4e86afa53d40482b45e2a6c67be04055137262c
Alessandra Souza <42846638+alelekime@users.noreply.github.com>
2021-03-12 19:59:51 -0300

    Create rainbowFiles.swift

---
+++ rainbowFiles.swift
@@ -0,0 +1,90 @@
+
+import SpriteKit
+import AVFoundation
+
+
+let album1 = (
+    pPoligono: Point(x: -325, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "dress 7.png")), 
+    pImagem: Point(x: 200, y: -200),
+    Color: UIColor(red: 1.00, green: 0.60, blue: 0.64, alpha: 1.00), 
+    music: "Little me"
+)
+let album2 = (
+    pPoligono: Point(x: -260, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 10.png")), 
+    pImagem: Point(x: 0, y: 0),
+    Color: UIColor(red: 1.00, green: 0.68, blue: 0.68, alpha: 1.00), 
+    music: "Let Her Go"
+)
+
+let album3 = (
+    pPoligono: Point(x: -195, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "dress 10.png")), 
+    pImagem: Point(x: 200, y: -200),
+    Color: UIColor(red: 1.00, green: 0.72, blue: 0.70, alpha: 1.00),
+    music: "Salute"
+)
+let album4 = (
+    pPoligono: Point(x: -130, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 7.png")), 
+    pImagem: Point(x: 150, y: 150),
+    Color: UIColor(red: 1.00, green: 0.79, blue: 0.73, alpha: 1.00), 
+    music: "I Found"
+)
+let album5 = (
+    pPoligono: Point(x: -65, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 8.png")), 
+    pImagem: Point(x: 0, y: -400),
+    Color: UIColor(red: 1.00, green: 0.85, blue: 0.76, alpha: 1.00), 
+    music: "Hurricane"
+)
+let album6 = (
+    pPoligono: Point(x: 0, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 6.png")), 
+    pImagem: Point(x: 0, y: -100),
+    Color: UIColor(red: 0.89, green: 0.94, blue: 0.80, alpha: 1.00),
+    music: "Broken"
+)
+
+let album7 = (
+    pPoligono: Point(x: 65, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 11.png")), 
+    pImagem: Point(x: 0, y: 0),
+    Color: UIColor(red: 0.80, green: 0.93, blue: 0.82, alpha: 1.00),
+    music: "Meant to Be"
+)
+let album8 = (
+    pPoligono: Point(x: 130, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 15.png")), 
+    pImagem: Point(x: 0, y: 0),
+    Color: UIColor(red: 0.71, green: 0.92, blue: 0.84, alpha: 1.00),
+    music: "You"
+)
+
+let album9 = (
+    pPoligono: Point(x: 195, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "dress 8.png")), 
+    pImagem: Point(x: 150, y: -200),
+    Color: UIColor(red: 0.75, green: 0.86, blue: 0.88, alpha: 1.00),
+    music: "Let"
+)
+
+let album10 = (
+    pPoligono: Point(x: 260, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 13.png")), 
+    pImagem: Point(x: 0, y: 0),
+    Color: UIColor(red: 0.78, green: 0.81, blue: 0.92, alpha: 1.00),
+    music: "Treat"
+)
+let album11 = (
+    pPoligono: Point(x: 325, y: 0),
+    image: Graphic(image:  imageLiteral(resourceName: "Ativo 5.png")), 
+    pImagem: Point(x: 50, y: -200),
+    Color: UIColor(red: 0.78, green: 0.75, blue: 0.88, alpha: 1.00),
+    music: "Break"
+)
+
+public let albums = [album1,album2, album3,album4,
+              album5,album6,album7,album8,
+              album9,album10,album11]
